b3d39050c13b68193b3841522eb9c31cc6c46c82
guigithub <guillaume.jacquart@abtasty.com>
2021-03-10 10:20:55 +0100

    Add boilerplate code for API synchronization

--- internal/files/api/syncFlags.go
+++ internal/files/api/syncFlags.go
@@ -1,9 +1,36 @@
 package api
 
-import "gitlab/canarybay/aws/integration/code-analyser/internal/files/model"
+import (
+	"gitlab/canarybay/aws/integration/code-analyser/internal/files/model"
+	"os"
+)
+
+// FlagInfo represent a flag code info sent to the Flagship API
+type FlagInfo struct {
+	FlagKey       string `json:"flagKey"`
+	RepositoryURL string `json:"repositoryUrl"`
+	FilePath      string `json:"filePath"`
+	LineNumber    int    `json:"lineNumber"`
+	Code          string `json:"code"`
+}
 
 // SendFlagsToAPI takes file search result & sends flag info to the API
-func SendFlagsToAPI([]model.FileSearchResult) (err error) {
-	// TODO
+func SendFlagsToAPI(results []model.FileSearchResult) (err error) {
+	flagInfos := []FlagInfo{}
+	for _, fr := range results {
+		for _, r := range fr.Results {
+			flagInfos = append(flagInfos, FlagInfo{
+				FlagKey:       r.FlagKey,
+				RepositoryURL: os.Getenv("REPOSITORY_URL"),
+				FilePath:      fr.File,
+				LineNumber:    r.LineNumber,
+				Code:          r.CodeLines,
+			})
+		}
+	}
+
+	// TODO : send call to API to route PUT /account_environments/{id}/tokens
+	// {id} should come from environment variable
+
 	return err
 }
