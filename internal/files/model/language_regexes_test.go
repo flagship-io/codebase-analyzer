c16d2a2d554079d1aa2d5c67ed89f7d323f4e736
guigithub <guillaume.jacquart@abtasty.com>
2021-04-09 11:35:02 +0200

    Add unit test for custom regex

---
+++ internal/files/model/language_regexes_test.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thoas/go-funk"
+)
+
+func TestCustomRegex(t *testing.T) {
+	AddCustomRegexes(`[{"extension_regexp":".tsx?","flag_regexes":[{"function_regex":"(?s)useFlagShipActivation\\(.+?\\)","key_regex":"\\s*['\"](.+?)['\"]","has_multiple_keys":true}]}]`)
+
+	found := funk.Find(LanguageRegexes, func(languageRegex LanguageRegex) bool {
+		return languageRegex.ExtensionRegex == ".tsx?"
+	})
+
+	assert.NotNil(t, found)
+	foundLanguageRegex, ok := found.(LanguageRegex)
+
+	assert.True(t, ok)
+	assert.NotNil(t, foundLanguageRegex)
+
+	assert.Equal(t, ".tsx?", foundLanguageRegex.ExtensionRegex)
+	assert.Equal(t, 1, len(foundLanguageRegex.FlagRegexes))
+	assert.Equal(t, "(?s)useFlagShipActivation\\(.+?\\)", foundLanguageRegex.FlagRegexes[0].FunctionRegex)
+	assert.Equal(t, "\\s*['\"](.+?)['\"]", foundLanguageRegex.FlagRegexes[0].KeyRegex)
+	assert.Equal(t, true, foundLanguageRegex.FlagRegexes[0].HasMultipleKeys)
+}
